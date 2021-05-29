93170928b707b5bd9a2e68457a4888df01e38dd5
Joey Willis <jwillis312@gmail.com>
2021-05-28 21:19:57 -0500

    Initial commit

---
+++ script/app.js
@@ -0,0 +1,56 @@
+const breakfast = document.querySelector('.breakfast-icon')
+const breakfastIcon = document.querySelector('.breakfast-icon p')
+const breakfastMenu = document.querySelector('.menu-grid')
+const lunch = document.querySelector('.lunch-icon')
+const lunchIcon = document.querySelector('.lunch-icon p')
+const lunchMenu = document.querySelector('.menu-grid')
+const dinner = document.querySelector('.dinner-icon')
+const dinnerIcon = document.querySelector('.dinner-icon p')
+const dinnerMenu = document.querySelector('.menu-grid')
+const openMenu = document.querySelector('.open-menu')
+const closeMenu = document.querySelector('.close-menu')
+
+
+openMenu.addEventListener('click', () => {
+    openMenu.classList.toggle('active');
+    closeMenu.classList.toggle('active');
+    document.querySelector('.main-nav').classList.toggle('active');
+
+});
+
+closeMenu.addEventListener('click', () => {
+    openMenu.classList.toggle('active');
+    closeMenu.classList.toggle('active');
+    document.querySelector('body').classList.toggle('active');
+    document.querySelector('.main-nav').classList.toggle('active');
+
+});
+
+
+
+breakfast.addEventListener('click', () => {
+    breakfastMenu.classList.add('breakfast');
+    breakfastIcon.classList.add('breakfast');
+    lunchMenu.classList.remove('lunch');
+    lunchIcon.classList.remove('lunch');
+    dinnerMenu.classList.remove('dinner');
+    dinnerIcon.classList.remove('dinner');
+} );
+
+lunch.addEventListener('click', () => {
+    lunchMenu.classList.add('lunch');
+    lunchIcon.classList.add('lunch');
+    breakfastMenu.classList.remove('breakfast');
+    breakfastIcon.classList.remove('breakfast');
+    dinnerMenu.classList.remove('dinner');
+    dinnerIcon.classList.remove('dinner');
+});
+
+dinner.addEventListener('click', () => {
+    dinnerMenu.classList.add('dinner');
+    dinnerIcon.classList.add('dinner');
+    breakfastMenu.classList.remove('breakfast');
+    breakfastIcon.classList.remove('breakfast');
+    lunchMenu.classList.remove('lunch');
+    lunchIcon.classList.remove('lunch');
+});
